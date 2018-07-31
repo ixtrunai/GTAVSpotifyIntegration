92718ac45851289529d02ee215d6b139c8439c91
ixtrunai <punkis96@gmail.com>
2018-07-31 19:11:01 +0200

    Primera subida

---
+++ SpotifyIntegrationMod/ScriptGTAV.cs
@@ -0,0 +1,34 @@
+﻿using GTA;
+using SpotifyIntegrationMod;
+using System;
+using System.Windows.Forms;
+
+public class ScriptGTAV : Script
+{
+    private Spotify spotify = new Spotify();
+
+    public ScriptGTAV()
+    {
+        Tick += OnTick;
+        Interval = 100; //cada 10 milisegundos
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+        if (Game.Player.Character.IsInVehicle())
+        {
+            ApagarRadio();
+            spotify.play();
+        }
+        else{
+            spotify.pause();
+        }
+    }
+
+    void ApagarRadio()
+    {
+        GTA.Vehicle veh = Game.Player.Character.CurrentVehicle;
+        veh.RadioStation = GTA.RadioStation.RadioOff;
+    }
+}
+  
---
+++ SpotifyIntegrationMod/Spotify.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAPI.Local; //Base Namespace
+using SpotifyAPI.Local.Enums; //Enums
+using SpotifyAPI.Local.Models; //Models for the JSON-responses
+
+namespace SpotifyIntegrationMod
+{
+    public class Spotify
+    {
+        //MULTIMEDIA KEYS UTILITIES
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+        private const int KeyeventfExtendedkey = 0x1;
+        private const int KeyeventfKeyup = 0x2;
+
+        //SPOTIFY
+        private static SpotifyLocalAPIConfig config = new SpotifyLocalAPIConfig { Port = 4381, HostUrl = "http://127.0.0.1" };
+        private static SpotifyLocalAPI spotify;
+        private static StatusResponse status;
+        Boolean playing = false;
+        
+        public Spotify()
+        {
+            //spotify init
+            spotify = new SpotifyLocalAPI(config);
+            spotify.Connect();
+            status = spotify.GetStatus();
+            if (!SpotifyLocalAPI.IsSpotifyRunning())
+            {
+                SpotifyLocalAPI.RunSpotify();
+            }
+
+            if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+            {
+                SpotifyLocalAPI.RunSpotifyWebHelper();
+            }
+        }
+
+        //siguiente (no usado)
+        public void next()
+        {
+            spotify.Skip();
+        }
+
+        //anterior (no usado)
+        public void previous()
+        {
+            spotify.Previous();
+        }
+
+        //parar
+        public void pause()
+        {
+            if (playing)
+            {
+                playing = false;
+                PressKey(0xb3);
+            }
+        }
+
+        //comenzar
+        public void play()
+        {
+            if (!playing)
+            {
+                playing = true;
+                PressKey(0xb3);
+            }
+        }
+
+        //SIMULAR PULSADO DE TECLA
+        internal void PressKey(byte keyCode)
+        {
+            keybd_event(keyCode, 0x45, KeyeventfExtendedkey, 0);
+            keybd_event(keyCode, 0x45, KeyeventfExtendedkey | KeyeventfKeyup, 0);
+        }
+    }
+}
